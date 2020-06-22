6051987b895d148c9c792196631cd43e1ff031b8
Allandt Bik-Elliott <allandt@gmail.com>
2020-06-22 11:41:05 +0100

    switch to using function component for Board with state hooks

--- src/board.js
+++ src/board.js
@@ -1,22 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TouchCircle from './touchCircle';
 import vars from './vars';
 
-export default class Board extends React.Component {
-    constructor(props) {
-        super(props);
+export default function Board(props) {
 
-        this.state = {
-            touchPoints: [ ],
-            countdownIntervalID: 0,
-            countdown: 0,
-        };
+    const [touchPoints, setTouchPoints] = useState([]);
 
-        this.handleTouchStart = this.handleTouchStart.bind(this);
-        this.handleTouchMove = this.handleTouchMove.bind(this);
-        this.handleTouchEnd = this.handleTouchEnd.bind(this);
-    }
-
+    /*
     componentDidMount() {
         // to stop the built in browser gesture interfering with the multitouch
         // events we need to turn off passive events for the touch which can
@@ -27,10 +17,11 @@ export default class Board extends React.Component {
         window.addEventListener('touchcancel', this.handleTouchEnd, { passive: false });
         window.addEventListener('touchmove', this.handleTouchMove, { passive: false });
     }
+    */
 
-    handleTouchStart(e) {
+    function handleTouchStart(e) {
         e.preventDefault();
-        const touchPoints = this.state.touchPoints.slice();
+        const tp = touchPoints.slice();
 
         // note e.changedTouches is a TouchList not an array
         // so we can't map over it
@@ -38,65 +29,67 @@ export default class Board extends React.Component {
             const touch = e.changedTouches[i];
             const angle = getAngleFromCenter(touch.pageX, touch.pageY);
 
-            touchPoints.push({
+            tp.push({
                 touch,
                 angle,
             });
         }
-        this.setState({ touchPoints });
+
+        setTouchPoints(tp);
     }
 
-    handleTouchMove(e) {
+    function handleTouchMove(e) {
         e.preventDefault();
-        const touchPoints = this.state.touchPoints.slice();
+        const tp = touchPoints.slice();
 
         for (var i = 0; i < e.changedTouches.length; i++) {
-            const touch = getTouchById(touchPoints, e.changedTouches[i].identifier);
+            const touch = getTouchById(tp, e.changedTouches[i].identifier);
             if (!touch) continue;
             touch.touch = e.changedTouches[i];
             touch.angle = getAngleFromCenter(touch.touch.pageX, touch.touch.pageY);
         }
 
-        this.setState({ touchPoints });
+        setTouchPoints(tp);
     }
 
-    handleTouchEnd(e) {
+    function handleTouchEnd(e) {
         e.preventDefault();
-        const touchPoints = this.state.touchPoints.slice();
+        const tp = touchPoints.slice();
 
         for (var i = 0; i < e.changedTouches.length; i++) {
-            const touch = getTouchById(touchPoints, e.changedTouches[i].identifier);
-            const index = touchPoints.indexOf(touch);
-            touchPoints.splice(index, 1);
+            const touch = getTouchById(tp, e.changedTouches[i].identifier);
+            const index = tp.indexOf(touch);
+            tp.splice(index, 1);
         }
 
-        this.setState({ touchPoints });
+        setTouchPoints(tp);
     }
 
-    // TODO add udpate if size changes using shouldComponentUpdate
-
-    render() {
-        const touchPoints = this.state.touchPoints.map(touchpoint =>
-            <TouchCircle 
-                key={ touchpoint.touch.identifier }
-                cx={ touchpoint.touch.pageX }
-                cy={ touchpoint.touch.pageY }
-                colour={ generateColour() }
-            />
-        );
-
-        return (
-            <svg 
-                xmlns={ vars.SVG_NS }
-                width={ window.innerWidth }
-                height={ window.innerHeight }
-            >
-                { touchPoints }
-            </svg>
-        );
-    }
+    return (
+        <svg 
+            xmlns={ vars.SVG_NS }
+            width={ window.innerWidth }
+            height={ window.innerHeight }
+            onTouchStart = { handleTouchStart }
+            onTouchMove = { handleTouchMove }
+            onTouchEnd = { handleTouchEnd }
+            onTouchCancel = { handleTouchEnd }
+        >
+            {
+                touchPoints.map(touchpoint =>
+                    <TouchCircle 
+                        key={ touchpoint.touch.identifier }
+                        cx={ touchpoint.touch.pageX }
+                        cy={ touchpoint.touch.pageY }
+                        colour={ generateColour() }
+                    />
+                )
+            }
+        </svg>
+    );
 }
 
+// lambadas
 const generateColour = () => vars.COLOURS[Math.ceil(Math.random() * vars.COLOURS.length) - 1];
 const getTouchById = (touchPoints, id) => touchPoints.filter(item => item.touch.identifier === id)[0];
 const getCenterPoint = () => {
