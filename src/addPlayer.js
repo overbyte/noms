076f9f658b19c4978849fae6443cc6c196e9200b
Allandt Bik-Elliott <allandt@gmail.com>
2020-07-06 11:56:59 +0100

    create addPlayer component

---
+++ src/addPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AddPlayer(props) {
+    return (
+        <div className='component chooseplayer'>
+            <h1>Add A Player</h1>
+        </div>
+    );
+}
