4fa9a0e7e54c2a1202617d17aa24b8f142e4dcfb
Allandt Bik-Elliott <allandt@gmail.com>
2020-07-07 15:57:00 +0100

    add nested route for Player and Game components

--- src/Noms.jsx
+++ src/Noms.jsx
@@ -3,100 +3,31 @@ import {
     HashRouter as Router,
     Switch,
     Route,
+    useRouteMatch,
 } from 'react-router-dom';
 import Board from './Board';
-import PlayerAdd from './PlayerAdd';
+import Game from './Game';
+import Player from './Player';
 import './Noms.css';
 
 export default function Noms() {
-    const routes = [
-        {
-            path: '/player/new',
-            component: () =>  (
-                <div className='playernew component'>
-                    <h1>add new player</h1>
-                </div>
-            )
-        },
-        {
-            path: '/player/existing',
-            component: () => (
-                <div className='playerexisting component'>
-                    <h1>add existing player</h1>
-                </div>
-            )
-        },
-        {
-            path: '/player/start',
-            component: () => (
-                <div className='playerstart component'>
-                    <h1>Choose a player to start</h1>
-                </div>
-            )
-        },
-        {
-            path: '/player',
-            component: PlayerAdd,
-        },
-        {
-            path: '/game/start',
-            component: () => (
-                <div className='gamestart component'>
-                    <h1>Game Start</h1>
-                </div>
-            )
-        },
-        {
-            path: '/game/nominate',
-            component: () => (
-                <div className='gamenominate component'>
-                    <h1>Game Nominate</h1>
-                </div>
-            )
-        },
-        {
-            path: '/game/play',
-            component: () => (
-                <div className='gameplay component'>
-                    <h1>Game play</h1>
-                </div>
-            )
-        },
-        {
-            path: '/game/over',
-            component: () => (
-                <div className='gameover component'>
-                    <h1>Game Over</h1>
-                </div>
-            )
-        },
-        {
-            path: '/game',
-            component: () => (
-                <div className='game component'>
-                    <h1>Game</h1>
-                </div>
-            )
-        },
-    ];
-
     return (
         <div className="noms">
             <Router>
                 <Switch>
-                    {
-                        routes.map(route => {
-                            return (
-                                <Route
-                                    path={ route.path }
-                                    component={ route.component }
-                                />
-                            );
-                        })
-                    }
+                    <Route 
+                        path="/player" 
+                        component={ Player }
+                    />
+                    <Route 
+                        path="/game" 
+                        component={ Game }
+                    />
                 </Switch>
             </Router>
+
             <Board />
         </div>
     )
 }
+
--- src/PlayerAdd.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PlayerAdd(props) {
-    return (
-        <div className='component chooseplayer'>
-            <h1>Add A Player</h1>
-        </div>
-    );
-}
