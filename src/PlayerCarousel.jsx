4203dee39c0d10b2d2452b271855d433bd660c53
Allandt Bik-Elliott <allandt@gmail.com>
2020-07-09 11:11:39 +0100

    css scroll snap the carousel and scroll to new name when one is added

--- src/PlayerCarousel.jsx
+++ src/PlayerCarousel.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PlayerCarouselItem from './PlayerCarouselItem';
 import './PlayerCarousel.css';
 
 export default function PlayerCarousel({ names }) {
+    const ref = useRef();
+
+    const handleClick = e => {
+        console.log(e);
+    };
+
+    useEffect(() => {
+        ref.current.scrollTo({ left: ref.current.scrollWidth, behavior: 'smooth' });
+    }, [names]);
+
     return (
-        <ul className="carousel">
+        <ul className="carousel" ref={ ref }>
             {
                 names.map((name, index) => (
-                    <li key={index} className='item'>
+                    <li 
+                        key={index}
+                        className='item'
+                        onClick={ handleClick }>
                         <PlayerCarouselItem 
                             name={name}
                         />
