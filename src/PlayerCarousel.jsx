02b44c8274dc2de899765eca0b6c7d6e62c6bee6
Allandt Bik-Elliott <allandt@gmail.com>
2020-07-09 14:24:51 +0100

    move PlayerCarouselItem into PlayerCarousel - it has no value as an external component

--- src/PlayerCarousel.jsx
+++ src/PlayerCarousel.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import PlayerCarouselItem from './PlayerCarouselItem';
+import React, {
+    useEffect,
+    useRef,
+    useContext,
+} from 'react';
+import { Store } from './Store';
 import './PlayerCarousel.css';
 
 export default function PlayerCarousel({ names }) {
@@ -13,12 +17,28 @@ export default function PlayerCarousel({ names }) {
         <ul className="carousel" ref={ ref }>
             {
                 names.map((name, index) => 
-                        <PlayerCarouselItem 
+                        <Item 
                             key={index}
                             name={name}
                         />
                 )
             }
         </ul>
+    );
+}
+
+function Item({ name }) {
+    const { dispatchNames } = useContext(Store);
+
+    const handleDeleteClick = e => dispatchNames({ type: 'NAME_REMOVE', data: name });
+    const handleSelectClick = e => dispatchNames({ type: 'NAME_SELECT', data: name });
+
+    return (
+        <li className='item'>
+            <img src={ `https://api.adorable.io/avatars/100/${name}.png` } alt={ name } />
+            <p>{name}</p>
+            <button onClick={ handleDeleteClick }>x</button>
+            <div className="selection" onClick={ handleSelectClick }></div>
+        </li>
     );
 }
